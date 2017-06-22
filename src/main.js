4b9450833d6a71467b16423c24b3a57f8168c31d
andrew yang <zzcv20051122@126.com>
2017-06-22 23:19:34 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Gallery from './Gallery';
+
+ReactDOM.render(
+  <Gallery />,
+  document.getElementById('root')
+);
