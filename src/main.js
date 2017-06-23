fe57505783b2f628e1c1eeff3d29ce0a21bdf787
andrew yang <zzcv20051122@126.com>
2017-06-23 08:14:45 +0800

    MISC - Bootstrapping Redux

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from './Gallery';
 
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
 ReactDOM.render(
-  <Gallery />,
+  <Provider store={store}>
+    <Gallery />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducer.js
@@ -0,0 +1,4 @@
+export default function images(state, action) {
+  console.log(state, action);
+  return state;
+}
