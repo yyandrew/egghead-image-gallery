533828c11d116db7dd2192d7ec211136088f63fb
andrew yang <zzcv20051122@126.com>
2017-06-24 09:18:54 +0800

    MISC - Configure redux-saga

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from './Gallery';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import { Provider } from 'react-redux';
 
 import { sayHello } from './saga';
-sayHello().next()
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  applyMiddleware(createSagaMiddleware(sayHello))
+);
 ReactDOM.render(
   <Provider store={store}>
     <Gallery />
